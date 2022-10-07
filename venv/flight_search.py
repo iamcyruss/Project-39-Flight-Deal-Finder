cfbb0662edf9157d67c7ebf2116df6cd77eaf6dc
Russ <russnicosia@gmail.com>
2022-10-07 15:59:35 -0600

    Updates

--- venv/flight_search.py
+++ venv/flight_search.py
@@ -2,25 +2,37 @@ import os
 import requests
 from datetime import date, timedelta
 from data_manager import DataManager
+from requests_toolbelt.utils import dump
 
 
 SHEETY_BEARER_TOKEN = os.environ.get("SHEETY_BEARER_TOKEN")
 DATA = DataManager.sheety_get(SHEETY_BEARER_TOKEN)['prices']
-print(DATA[0]['city'])
-TODAY = date.today().strftime('%m/%d/%Y')
-NINETY_FROM_TODAY = (date.today() + timedelta(days=90)).strftime('%m/%d/%Y')
-ONETWENTY_FROM_TODAY = (date.today() + timedelta(days=120)).strftime('%m/%d/%Y')
-print(f"Todays date: {TODAY}\n90 days from now: {NINETY_FROM_TODAY}\n120 days from now: {ONETWENTY_FROM_TODAY}")
+TODAY = date.today().strftime('%d/%m/%Y')
+NINETY_FROM_TODAY = (date.today() + timedelta(days=90)).strftime('%d/%m/%Y')
+ONETWENTY_FROM_TODAY = (date.today() + timedelta(days=120)).strftime('%d/%m/%Y')
+KIWI_API_KEY = os.environ.get("KIWI_API_KEY")
+KIWI_HEADERS = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+    "apikey": KIWI_API_KEY
+}
 
 for i in DATA:
     KIWI_PAYLOAD = {
-        "fly_from": 'Denver',
-        "fly_to": i['city'],
-        "dateFrom": NINETY_FROM_TODAY,
-        "dateTo": ONETWENTY_FROM_TODAY
+        "fly_from": 'DEN',
+        "fly_to": i['iataCode'],
+        "date_from": NINETY_FROM_TODAY,
+        "date_to": ONETWENTY_FROM_TODAY,
+        "curr": "USD",
+        "limit": 5
     }
-    print(KIWI_PAYLOAD)
-    KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search?fly_from=LGA&fly_to=MIA&dateFrom=01/04/2021&dateTo=02/04/2021"
+    KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search/"
+    kiwi_response = requests.get(url=KIWI_ENDPOINT, headers=KIWI_HEADERS, params=KIWI_PAYLOAD)
+    #kiwi_response.raise_for_status()
+    kiwi_json = kiwi_response.json()
+    print(kiwi_json['data'])
+    for price in kiwi_json['data']:
+        print(price['price'])
 
 
 class FlightSearch:
