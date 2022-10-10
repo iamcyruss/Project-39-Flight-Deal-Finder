57721fb5cbb9b9b52c651722a04319fe618d4ee1
Russ <russnicosia@gmail.com>
2022-10-10 14:24:46 -0600

    update-10/10/22

--- venv/flight_search.py
+++ venv/flight_search.py
@@ -17,28 +17,56 @@ KIWI_HEADERS = {
 }
 
 for i in DATA:
-    KIWI_PAYLOAD = {
+    KIWI_FLYTO_PAYLOAD = {
         "fly_from": 'DEN',
         "fly_to": i['iataCode'],
         "date_from": NINETY_FROM_TODAY,
         "date_to": ONETWENTY_FROM_TODAY,
         "curr": "USD",
+        "locale": "en",
+        "limit": 5
+    }
+    KIWI_FLYBACK_PAYLOAD = {
+        "fly_from": i['iataCode'],
+        "fly_to": "DEN",
+        "date_from": NINETY_FROM_TODAY,
+        "date_to": ONETWENTY_FROM_TODAY,
+        "curr": "USD",
+        "locale": "en",
         "limit": 5
     }
     KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search/"
-    kiwi_response = requests.get(url=KIWI_ENDPOINT, headers=KIWI_HEADERS, params=KIWI_PAYLOAD)
-    #kiwi_response.raise_for_status()
-    kiwi_json = kiwi_response.json()
-    print(kiwi_json['data'])
-    for count, price in enumerate(kiwi_json['data']):
-        if len(price['route']) == 1 and count == 0:
-            print(f"Price: ${price['price']}\nBag Prices: {price['bags_price']}\n"
-                  f"From: {price['route'][0]['cityFrom']} To: {price['route'][0]['cityTo']}\n"
-                  f"Depart: {price['route'][0]['utc_departure']}\nArrive: {price['route'][0]['utc_arrival']}\n"
+    kiki_flyto_response = requests.get(url=KIWI_ENDPOINT, headers=KIWI_HEADERS, params=KIWI_FLYTO_PAYLOAD)
+    kiki_flyto_response.raise_for_status()
+    kiwi_flyto_json = kiki_flyto_response.json()
+    kiwi_flyback_response = requests.get(url=KIWI_ENDPOINT, headers=KIWI_HEADERS, params=KIWI_FLYBACK_PAYLOAD)
+    kiwi_flyback_response.raise_for_status()
+    kiwi_flyback_json = kiwi_flyback_response.json()
+    print(f"Fly To Raw Data: {kiwi_flyto_json['data']}")
+    print(f"Fly Back Raw Data:{kiwi_flyback_json['data']}")
+    for count, price_to in enumerate(kiwi_flyto_json['data']):
+        if len(price_to['route']) == 1 and count == 0:
+            print(f"Price To: ${price_to['price']} \nBag Prices To: {price_to['bags_price']}\n"
+                  f"From: {price_to['route'][0]['cityFrom']} To: {price_to['route'][0]['cityTo']}\n"
+                  f"Depart: {price_to['route'][0]['utc_departure']}\nArrive: {price_to['route'][0]['utc_arrival']}\n"
                   f"-------------------------------------------------------------------")
-        elif len(price['route']) > 1:
-            print(f"Price: ${price['price']}\nBag Prices: ${price['bags_price']}")
-            for route_count, route in enumerate(price['route']):
+        elif len(price_to['route']) > 1:
+            print(f"Price To: ${price_to['price']}\nBag Prices To: ${price_to['bags_price']}")
+            for route_count, route in enumerate(price_to['route']):
+                print(f"Stop {route_count + 1}: From: {route['cityFrom']} To: {route['cityTo']}\n"
+                      f"Depart: {route['utc_departure']}\nArrive: {route['utc_arrival']}\n"
+                      f"-------------------------------------------------------------------")
+        else:
+            pass
+    for count, price_back in enumerate(kiwi_flyback_json['data']):
+        if len(price_back['route']) == 1 and count == 0:
+            print(f"Price Back: ${price_back['price']} \nBag Prices Back: {price_back['bags_price']}\n"
+                  f"From: {price_back['route'][0]['cityFrom']} To: {price_back['route'][0]['cityTo']}\n"
+                  f"Depart: {price_back['route'][0]['utc_departure']}\nArrive: {price_back['route'][0]['utc_arrival']}\n"
+                  f"-------------------------------------------------------------------")
+        elif len(price_back['route']) > 1:
+            print(f"Price Back: ${price_back['price']}\nBag Prices Back: ${price_back['bags_price']}")
+            for route_count, route in enumerate(price_back['route']):
                 print(f"Stop {route_count + 1}: From: {route['cityFrom']} To: {route['cityTo']}\n"
                       f"Depart: {route['utc_departure']}\nArrive: {route['utc_arrival']}\n"
                       f"-------------------------------------------------------------------")
