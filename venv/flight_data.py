9449953c76a648d5b4f715e135ca733827811fa5
Russ <russnicosia@gmail.com>
2022-10-17 18:52:16 -0600

    update-10/17/22

--- venv/flight_data.py
+++ venv/flight_data.py
@@ -7,40 +7,60 @@ class FlightData:
     def flight_data_formatter(self, i, flight_data_json):
 
         print(f"Fly To Raw Data: {flight_data_json['data']}")
-        print(f"Fly Back Raw Data:{flight_data_json['data']}")
         for count, price_to in enumerate(flight_data_json['data']):
+            departure_date = price_to['route'][0]['utc_departure'].split('T')
+            departure_time = departure_date[1].split('.')
+            #print(f"Departure dates: {departure_date[0]} {departure_time[0]}")
+            arrival_date = price_to['route'][0]['utc_arrival'].split('T')
+            arrival_time = arrival_date[1].split('.')
+            #print(f"Arrival dates: {arrival_date[0]} {arrival_time[0]}")
             if len(price_to['route']) == 1 and count == 0:
-                print(
-                    f"Price To: ${price_to['price']} Low Price: ${i['lowestPrice']}\nBag Prices To: {price_to['bags_price']}\n"
+                """print(
+                    f"Price To: ${price_to['price']} Low Price: ${i['lowestPrice']}\n"
+                    f"Bag Prices To: {price_to['bags_price']}\n"
                     f"From: {price_to['route'][0]['cityFrom']} To: {price_to['route'][0]['cityTo']}\n"
                     f"Depart: {price_to['route'][0]['utc_departure']}\nArrive: {price_to['route'][0]['utc_arrival']}\n"
-                    f"-------------------------------------------------------------------")
+                    f"-------------------------------------------------------------------")"""
                 if int(price_to['price']) < int(i['lowestPrice']):
                     sms_list = f"-\nFrom: {price_to['route'][0]['cityFrom']}\n" \
                                f"To: {price_to['route'][0]['cityTo']}\n" \
                                f"Price: ${price_to['price']}\n" \
-                               f"Depart: {price_to['route'][0]['utc_departure']}\n" \
-                               f"Arrive: {price_to['route'][0]['utc_arrival']}\n" \
+                               f"Depart: {departure_date[0]} {departure_time[0]}\n" \
+                               f"Arrive: {arrival_date[0]} {arrival_time[0]}\n" \
                                f"-------------------------------\n"
-                    NotificationManager.send_sms(self, sms_list)
+                    print(f"sms_list: {sms_list}")
+                    #NotificationManager.send_sms(self, sms_list)
             elif len(price_to['route']) > 1:
-                print(
-                    f"Price To: ${price_to['price']} Low Price: ${i['lowestPrice']}\nBag Prices To: ${price_to['bags_price']}")
-                sms_list = []
+                #print(f"Price To: ${price_to['price']} Low Price: ${i['lowestPrice']}\n"
+                      #f"Bag Prices To: ${price_to['bags_price']}")
+                sms_list = ''
                 for route_count, route in enumerate(price_to['route']):
-                    print(f"Stop {route_count + 1}: From: {route['cityFrom']} To: {route['cityTo']}\n"
-                          f"Depart: {route['utc_departure']}\nArrive: {route['utc_arrival']}\n"
-                          f"-------------------------------------------------------------------")
+                    #print(f"Stop {route_count + 1}: From: {route['cityFrom']} To: {route['cityTo']}\n"
+                          #f"Depart: {route['utc_departure']}\nArrive: {route['utc_arrival']}\n"
+                          #f"-------------------------------------------------------------------")
+                    departure_date = price_to['route'][route_count]['utc_departure'].split('T')
+                    departure_time = departure_date[1].split('.')
+                    # print(f"Departure dates: {departure_date[0]} {departure_time[0]}")
+                    arrival_date = price_to['route'][route_count]['utc_arrival'].split('T')
+                    arrival_time = arrival_date[1].split('.')
+                    # print(f"Arrival dates: {arrival_date[0]} {arrival_time[0]}")
                     if int(price_to['price']) < int(i['lowestPrice']):
-                        sms_list.append(f"-\nFrom: {price_to['route'][0]['cityFrom']}\n"
-                                        f"To: {price_to['route'][0]['cityTo']}\n"
-                                        f"Price: ${price_to['price']}\n"
-                                        f"Depart: {price_to['route'][0]['utc_departure']}\n"
-                                        f"Arrive: {price_to['route'][0]['utc_arrival']}\n"
-                                        f"-------------------------------\n")
-                    if len(sms_list) > 1:
-                        NotificationManager.send_sms(self, sms_list)
-                    else:
-                        pass
+                        sms_list = f"{sms_list}\nStop {route_count + 1}:\n" \
+                                   f"From: {price_to['route'][route_count]['cityFrom']}\n" \
+                                   f"To: {price_to['route'][route_count]['cityTo']}\n" \
+                                   f"Price: ${price_to['price']}\n" \
+                                   f"Depart: {departure_date[0]} {departure_time[0]}\n" \
+                                   f"Arrive: {arrival_date[0]} {arrival_time[0]}\n" \
+                                   f"-------------------------------\n"
+                if len(sms_list) > 0 and flight_data_json['data'][0]['flyFrom'] == 'DEN':
+                    print(f"Departing Flights: {sms_list}")
+                    #print(len(sms_list))
+                    #NotificationManager.send_sms(self, sms_list=f"Departing Flights: {sms_list}")
+                elif len(sms_list) > 0 and flight_data_json['data'][0]['flyTo'] == 'DEN':
+                    print(f"Returning Flights: {sms_list}")
+                    #print(len(sms_list))
+                    #NotificationManager.send_sms(self, sms_list=f"Returning Flights: {sms_list}")
+                else:
+                    pass
             else:
                 pass
